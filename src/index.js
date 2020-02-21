f4672ed674735dcff3c18f0736d518f58fa8d997
romyaudio <romyaudio@hotmail.com>
2020-02-20 22:32:20 -0500

    4

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Card from './components/card';
 import 'bootstrap/dist/css/bootstrap.css'
-import circlesImg from './img/exercise.png'
-import Welcome from './components/Welcome'
+import Exercises from './pages/Exercises'
+
 
 
 // ReactDOM.render(__QUE__,__DONDE__)
-ReactDOM.render(<div>
-	            <Welcome
-	               username="Romy"
-
-	               />
-	               
-                 <Card
-	                title="Title"
-	                description="Description"
-	                img={circlesImg}
-	                leftColor="#A74CF2"
-	                rightColor="#617BFB"
-	            />
-	           </div>,document.getElementById('root'))
+ReactDOM.render(<Exercises/>,document.getElementById('root'))
 
